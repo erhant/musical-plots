1b05f79f0425ad34a01b4c1ae6a7976c39254f00
Erhan Tezcan <erhant96@hotmail.com>
2021-06-07 21:21:18 +0300

    added scales

---
+++ src/intervals.py
@@ -0,0 +1,16 @@
+
+
+Root = [0]
+Minor2 = [1]
+Major2 = [2]
+Minor3 = [3]
+Major3 = [4]
+Perfect4 = [5]
+Tritone = [6]
+Flat5 = Tritone
+Perfect5 = [7]
+Minor6 = [8]
+Major6 = [9]
+Minor7 = [10]
+Major7 = [11]
+Octave = [12]
